5e51da888b25fd9158847c48ce3e25c40367d237
mrigank-newton <mrigank7903710344@gmail.com>
2021-03-24 22:21:12 +0530

    Protected Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,35 @@
-import './../App.css';
-import React, { useEffect, useState } from 'react';
+import "./../App.css";
+import React, { useEffect, useState } from "react";
 import ClassComponent from "./ClassComponent";
 import FunctionalComponent from "./FunctionalComponent";
-import { ToastContainer } from 'react-toastify';
-import Home from './Home';
-import Login from './Login';
-import SignUp from './SignUp';
-import {Route, Redirect, Switch} from 'react-router-dom';
-import  AppBar  from './AppBar';
-import AddCourseForm from './AddCourseForm';
-import 'react-toastify/dist/ReactToastify.css';
-import CourseDetails from './CourseDetails';
-import MyCourses from './MyCourses';
-import authService from '../service/auth.service';
-import ProtectedRoute from './common/ProtectedRoute';
+import { ToastContainer } from "react-toastify";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { Route, Redirect, Switch } from "react-router-dom";
+import AppBar from "./AppBar";
+import AddCourseForm from "./AddCourseForm";
+import "react-toastify/dist/ReactToastify.css";
+import CourseDetails from "./CourseDetails";
+import MyCourses from "./MyCourses";
+import authService from "../service/auth.service";
+import ProtectedRoute from "./common/ProtectedRoute";
 
 function App() {
-
   return (
     <>
-      <ToastContainer/>
-      <AppBar/>
+      <ToastContainer />
+      <AppBar />
       <Switch>
-          <Route path='/home' component={Home}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/signup' component={SignUp}/>
-          <ProtectedRoute path='/add-course' component={AddCourseForm} {...props}/>
-          <Route path='/all-courses' component={CourseDetails}/>
-          <Route path='/my-courses' component={MyCourses}/>
-          <Redirect to='/login'/>
+        <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <ProtectedRoute path='/add-course' component={AddCourseForm}/>
+        <Route path="/all-courses" component={CourseDetails} />
+        <Route path="/my-courses" component={MyCourses} />
+        <Redirect to="/login" />
       </Switch>
     </>
-    
   );
 }
 
--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,11 +1,14 @@
+import { Redirect, Route } from "react-router";
 import authService from "../../service/auth.service";
 
 const ProtectedRoute = ({component: Component, path}) => {
-    if(authService.isLoggedIn() && authService.isAdmin()) {
-        return <Route path={path} component={Component}/>
-    } else {
-        return <Redirect to='/login'/>
-    }
+     return (<Route path={path} render={() => {
+         if(authService.isLoggedIn() && authService.isAdmin()) {
+             return <Component/>
+         } else {
+             return <Redirect to='/login'/>
+         }
+        }}/>)
 }
- 
+
 export default ProtectedRoute;
